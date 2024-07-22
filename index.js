1024b076b9e3c8fb53a40b9833f81f42ddf4de3d
Robson Ferreira dos Santos Junior <robson.santos.rs27@gmail.com>
2024-07-22 14:10:47 -0300

    Clean code optimization

--- index.js
+++ index.js
@@ -1,57 +1,81 @@
 const turingMachine = require('./turingMachine');
 
-function processarEntrada (word, turingMachine) {
+function processarEntrada(fitaDeEntrada, turingMachine) {
   
-  fitaEsquerda = turingMachine.vazio;
-  // todo: verificar se word pertence ao alfabeto de entrada
-  fitaDireita = word + turingMachine.vazio;
-  estadoAtual = turingMachine.estadoInicial;
-  
-  console.log(fitaEsquerda, estadoAtual, fitaDireita);
-
-  transicaoSimboloLido = selecionarTransicao(estadoAtual, turingMachine.funcaoTransicao, fitaDireita[0]);
-
-  let resultadoMovimento;
-
-  while (transicaoSimboloLido) {
-    estadoFita = {
-      transicao: transicaoSimboloLido,
-      fitaEsquerda: fitaEsquerda,
-      fitaDireita: fitaDireita,
-      estadoAtual: estadoAtual
-    };
-
-    if (transicaoSimboloLido.movimento == "D") {
-      resultadoMovimento = movimentoDireita(estadoFita);
-    }
-    else {
-      resultadoMovimento = movimentoEsquerda(estadoFita);
-    }
-
-    fitaDireita = resultadoMovimento.fitaDireita;
-    fitaEsquerda = resultadoMovimento.fitaEsquerda;
-    estadoAtual = resultadoMovimento.estadoAtual;
-
-    transicaoSimboloLido = selecionarTransicao(estadoAtual, turingMachine.funcaoTransicao, fitaDireita[0]);
-    console.log(fitaEsquerda, estadoAtual, fitaDireita);
+  data = {
+    fitaEsquerda: turingMachine.vazio,
+    fitaDireita: fitaDeEntrada + turingMachine.vazio,
+    estadoAtual: turingMachine.estadoInicial,
+    turingMachine: turingMachine
   }
 
-  if (turingMachine.estadosFinais.includes(estadoAtual)) {
+  estadoFinal = executeTuringMachine(data);
+
+  if (turingMachine.estadosFinais.includes(estadoFinal)) {
     console.log("aceita");
-  }
-  else {
+  } else {
     console.log("rejeita");
   }
 }
 
-function selecionarTransicao (estadoAtual, funcaoTransicao, simboloLido) {
-  transicoesEstadoAtual = funcaoTransicao.filter(obj => obj.estadoAtual === estadoAtual);
-  transicaoSimboloLido = transicoesEstadoAtual.filter(obj => obj.simboloLido === simboloLido);
+function executeTuringMachine(data) {
+  
+  auxObj = {
+    estadoAtual: data.estadoAtual, 
+    funcaoTransicao: data.turingMachine.funcaoTransicao, 
+    simboloLido: data.fitaDireita[0]
+  };
+  
+  let transicaoSimboloLido = selecionarTransicao(auxObj);
+
+  console.log(data.fitaEsquerda, data.estadoAtual, data.fitaDireita);
+  
+  while (transicaoSimboloLido) {
+    const estadoFita = {
+      transicao: transicaoSimboloLido,
+      fitaEsquerda: data.fitaEsquerda,
+      fitaDireita: data.fitaDireita,
+      vazio: data.turingMachine.vazio
+    };
+
+    let resultadoMovimento = decidirMovimento(transicaoSimboloLido, estadoFita);
+
+    data.fitaDireita = resultadoMovimento.fitaDireita;
+    data.fitaEsquerda = resultadoMovimento.fitaEsquerda;
+    data.estadoAtual = transicaoSimboloLido.novoEstado;
+    
+    auxObj.estadoAtual = data.estadoAtual;
+    auxObj.simboloLido = data.fitaDireita[0];
+
+    console.log(data.fitaEsquerda, data.estadoAtual, data.fitaDireita);
+
+    transicaoSimboloLido = selecionarTransicao(auxObj);
+  }
+
+  return data.estadoAtual
+}
+
+function decidirMovimento (transicaoSimboloLido, estadoFita) {
+  
+  let resultadoMovimento;
+
+  if (transicaoSimboloLido.movimento == "D") {
+    resultadoMovimento = movimentoDireita(estadoFita);
+  } else {
+    resultadoMovimento = movimentoEsquerda(estadoFita);
+  }
+
+  return resultadoMovimento;
+}
+
+function selecionarTransicao (estadoFita) { 
+  transicoesEstadoAtual = estadoFita.funcaoTransicao.filter(obj => obj.estadoAtual === estadoFita.estadoAtual);
+  transicaoSimboloLido = transicoesEstadoAtual.filter(obj => obj.simboloLido === estadoFita.simboloLido);
 
   return transicaoSimboloLido[0];
 }
 
-function movimentoEsquerda (estadoFita) {
+function movimentoEsquerda (estadoFita) { 
   if (estadoFita.fitaEsquerda == "") {
     return; //erro
   }
@@ -63,18 +87,15 @@ function movimentoEsquerda (estadoFita) {
 
   estadoFita.fitaEsquerda = estadoFita.fitaEsquerda.slice(0, -1);
 
-  estadoFita.estadoAtual = estadoFita.transicao.novoEstado
-
   return estadoFita;
 }
 
-function movimentoDireita (estadoFita) {
+function movimentoDireita (estadoFita) { 
   estadoFita.fitaEsquerda += estadoFita.transicao.novoSimbolo;
   estadoFita.fitaDireita = estadoFita.fitaDireita.slice(1);
-  estadoFita.estadoAtual = estadoFita.transicao.novoEstado;
   
   if (estadoFita.fitaDireita == "") {
-    estadoFita.fitaDireita = "B";
+    estadoFita.fitaDireita = estadoFita.vazio;
   }
   
   return estadoFita;
